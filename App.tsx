8e7ad36884ec7b02da7ba25dcbf1758dd81b0d92
Laura Pana <laurapana@MacBook-Pro.local>
2022-09-08 15:38:13 +0100

    Navigation added

--- App.tsx
+++ App.tsx
@@ -1,21 +1,54 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import TestComponent from './src/components/TestComponent';
+import HomeScreen from './src/screens/Home/Home';
+import NoteDetails from './src/screens/NoteDetails/NoteDetails';
 
 
-const App = () => {
-  const test = false
+const Stack = createNativeStackNavigator();
+const App: () => Node = () => {
+  // const test = false
 
-  let name: string | null = 'Laura';
+  // let name: string | null = 'Laura';
 
-  name = null
+  // name = null
 
   return (
-    <ScrollView>
-      <TestComponent
-        containerStyle={[{ flex: 1 }, test && { borderBottomWidth: 1 }]}
-        textStyle={{ fontSize: 40, borderStyle: 'solid' }} color='#000000' title={{ name: 'Test' }}></TestComponent>
-    </ScrollView>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackVisible: true,
+          headerBackTitleVisible: false,
+          headerTitleStyle: {
+            color: '#3A96EB',
+            fontSize: 24,
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="NoteDetails" component={NoteDetails}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
+    // <ScrollView>
+    //   <TestComponent
+    //     containerStyle={[{ flex: 1 }, test && { borderBottomWidth: 1 }]}
+    //     textStyle={{ fontSize: 40, borderStyle: 'solid' }} color='#000000' title={{ name: 'Test' }}></TestComponent>
+    //   <TemplateText>
+    //     bbbbbbbbb
+    //     <TemplateText style={{ color: 'red' }} onPress={() => console.log('Laura')}>
+    //       bgujuybjvjvy
+    //     </TemplateText>
+    //     aaaaaaaaaaa
+    //   </TemplateText>
+    //   <TemplateIcon name='add' family='Ionicons' size={20} color='#000000' ></TemplateIcon>
+    // </ScrollView>
   );
 };
 
